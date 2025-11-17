5638e05a5579d7253921fe025e3327594dceb9b4
Maryam Shafiq <maryam.shafiq219@yahoo.com>
2025-11-17 11:28:51 +0000

    Add About app with model, views, and templates; update navigation in base.html

---
+++ about/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+
+def about_me(request):
+    """
+    Display the About page content.
+
+    **Context**
+
+    ``about``
+        An instance of :model: `about.About`
+
+    **Template:**
+
+    :template:`about/about.html`
+    """
+
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(request, "about/about.html", {"about": about})
